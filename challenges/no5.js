a2d8c9a32aee4ddfcf796bf7cbfd57b602ea3075
W. Yang-Lee Young <celalkincross@gmail.com>
2023-01-03 06:14:41 +0800

    refactor code no5

--- challenges/no5.js
+++ challenges/no5.js
@@ -20,17 +20,26 @@ const m2 = [
     [2, 3, 4, 5]
 ]
 
+// problem will b approached in three parts after initializing row-major search:
+// Part 1: row 0 not last column--check values ahead 
+// Part 2: last column
+// Part 3: all other rows
 function isMatrixMonotonic(matrix) {
     /* your code here */
     for (let r=0; r < matrix.length; row++) {
-        for (let c=0; c < matrix[r].length; c++) {
+        const rowLength = matrix[r].length-1
+        for (let c=0; c < rowLength; c++) {
+            // save value
             const value = matrix[r][c];
-
-            if (r == 0 &&  c < matrix[r].length-1 && matrix[r][c]>matrix[r][c +1]) {
+            const next = matrix[r][c+1];
+            const prevUp = matrix[r-1][c];
+            
+            // Part 1: row 0 not last column--check value ahead 1
+            if (r == 0 &&  c < rowLength && value>next) {
                 return false;
-            } else if (c == matrix[r].length-1 && r>0 && matrix[r][c]> matrix[r][c-1]) {
+            } else if (c == rowLength && r>0 && value> prevUp) {
                 return false;
-            } else if (r > 0 &&  c < matrix[r].length-1 && (matrix[r][c]>matrix[r][c +1] || matrix[r][c]< matrix[r-1][c])) {
+            } else if (r > 0 &&  c < rowLength && (value>next || value< prevUp)) {
                 return false;
             }
 
