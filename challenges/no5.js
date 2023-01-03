99ccf85a450cdd16a8033b5951ef40b408edeff3
W. Yang-Lee Young <celalkincross@gmail.com>
2023-01-03 19:57:08 +0800

    finish code no5

--- challenges/no5.js
+++ challenges/no5.js
@@ -26,23 +26,58 @@ const m2 = [
 // Part 3: all other rows
 function isMatrixMonotonic(matrix) {
     /* your code here */
-    for (let r=0; r < matrix.length; row++) {
-        const rowLength = matrix[r].length-1
+    let next, prevUp;
+    for (let r=0; r < matrix.length; r++) {
+        const rowLength = matrix[r].length-1;
         for (let c=0; c < rowLength; c++) {
             // save value
             const value = matrix[r][c];
-            const next = matrix[r][c+1];
-            const prevUp = matrix[r-1][c];
+            console.log({"matrix[r][c+1]": matrix[r][c+1]})
+            if (c<rowLength) {next = matrix[r][c+1];}
             
+
+            if (r>1) {const prevUp = matrix[r-1][c];}
             // Part 1: row 0 not last column--check value ahead 1
-            if (r == 0 &&  c < rowLength && value>next) {
+            console.log({r: r, c: c, next: next, rowlength: rowLength})
+            if (r == 0 &&  c < rowLength-1 && value > next) {
                 return false;
+
+            // Part 2: last column
             } else if (c == rowLength && r>0 && value> prevUp) {
                 return false;
+
+            // Part 3: all other rows
             } else if (r > 0 &&  c < rowLength && (value>next || value< prevUp)) {
                 return false;
             }
-
         }
     }
+    return true
 }
+
+console.log(isMatrixMonotonic(m2));
+
+//Solution Works!
+
+// Of course, Formation's solution is far more elegant and succint 
+
+function isMatrixMonotonicAns(matrix) {
+
+    // row-major traversal over matrix
+    for (let r=0; r<matrix.length; r++) {
+        for (let c=0; c<matrix[r].length; c++) {
+            const value= matrix[r][c];
+            // check value on the second row and later with the value above it
+            if (r>0 && matrix[r-1][c] > value) return false;
+            
+            // check column on second, column and beyond it with value to left
+            if (c>0 && matrix[r][c-1] > value) return false;
+        }
+    }
+
+    // else return true 
+
+    return true;
+}
+
+console.log({Formation: isMatrixMonotonicAns(m2)})
